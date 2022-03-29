243f89aef9dc4e4e72a1f30649c5543013843596
Dan Okkels Brendstrup <dob@eaaa.dk>
2022-03-29 21:36:00 +0200

    fix: correctly center placeholder text

--- app/routes/snippets/index.jsx
+++ app/routes/snippets/index.jsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <p className="text-sm text-center text-slate-400 self-center">
+    <p className="grid h-full place-items-center text-sm text-center text-slate-400">
       No snippet selected
     </p>
   );
