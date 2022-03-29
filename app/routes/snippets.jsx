0581d8f875ff9a85405647282174ad153e1f8da0
Dan Okkels Brendstrup <dob@eaaa.dk>
2022-03-29 15:02:31 +0200

    feat: use SVG icons from Heroicons

--- app/routes/snippets.jsx
+++ app/routes/snippets.jsx
@@ -23,8 +23,20 @@ export default function Index() {
           </h1>
           <Link
             to="/snippets/new"
-            className="font-bold text-2xl text-slate-400 hover:bg-slate-100 transition-colors p-4 border-l border-slate-200">
-            +
+            className="text-slate-400 hover:text-slate-600 transition-colors p-4 border-l border-slate-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4v16m8-8H4"
+              />
+            </svg>
           </Link>
         </div>
         <ul>
@@ -34,14 +46,29 @@ export default function Index() {
                 <Link
                   to={snippet._id}
                   className={[
-                    "block p-4 hover:bg-slate-100 transition-colors border-slate-200 flex flex-row justify-between items-center",
+                    "p-4 hover:bg-slate-100 transition-colors border-slate-200 flex flex-row justify-between items-center",
                     i > 0 && "border-t",
                   ]
                     .filter(Boolean)
                     .join(" ")}>
                   <span>{snippet.title}</span>
-                  <span className="text-slate-400">
-                    {snippet.favorite ? "★" : "☆"}
+                  <span
+                    className={[
+                      snippet.favorite ? "text-amber-500" : "text-slate-400",
+                    ]}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke={snippet.favorite ? "none" : "currentColor"}
+                      fill={snippet.favorite ? "currentColor" : "none"}>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                      />
+                    </svg>
                   </span>
                 </Link>
               </li>
--- app/routes/snippets/$snippetId.jsx
+++ app/routes/snippets/$snippetId.jsx
@@ -31,15 +31,30 @@ export default function SnippetPage() {
   const snippet = useLoaderData();
   return (
     <div>
-      <div className="flex flex-row items-start justify-between">
+      <div className="flex flex-row items-center justify-between">
         <div className="flex flex-row items-center mb-4">
           <Form method="post">
             <button
               name="_action"
               value="favorite"
               type="submit"
-              className="mr-2 text-slate-400 hover:text-slate-700 transition-colors text-xl">
-              {snippet.favorite ? "★" : "☆"}
+              className={[
+                "block mr-2 hover:text-slate-700 transition-colors",
+                snippet.favorite ? "text-amber-500" : "text-slate-400",
+              ].join(" ")}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke={snippet.favorite ? "none" : "currentColor"}
+                fill={snippet.favorite ? "currentColor" : "none"}>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                />
+              </svg>
             </button>
           </Form>
           <h1 className="text-2xl font-bold">{snippet.title}</h1>
@@ -51,8 +66,20 @@ export default function SnippetPage() {
             type="submit"
             title="Delete"
             aria-label="Delete"
-            className="font-semibold text-xl text-white w-7 h-7 bg-red-500 transition-colors hover:bg-red-600 rounded">
-            &times;
+            className="transition-colors text-slate-400 hover:text-red-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
           </button>
         </Form>
       </div>
