a7e4909cc96f53adb7c316143bfc297fa1783eaa
Dan Okkels Brendstrup <dob@eaaa.dk>
2022-04-04 20:40:42 +0200

    refactor: use icons directly from @heroicons pkg

--- app/routes/snippets.jsx
+++ app/routes/snippets.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { SearchIcon, PlusIcon, StarIcon } from "@heroicons/react/outline";
 import {
   Outlet,
   useLoaderData,
@@ -52,19 +53,7 @@ export default function Index() {
           <Link
             to="/snippets/new"
             className="text-slate-400 hover:text-slate-600 transition-colors p-4 border-l border-slate-200">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4v16m8-8H4"
-              />
-            </svg>
+            <PlusIcon className="h-6 w-6" />
           </Link>
         </div>
         <Form
@@ -83,19 +72,7 @@ export default function Index() {
           <button
             type="submit"
             className="px-4 py-2 text-slate-400 hover:text-slate-600 transition-colors">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
+            <SearchIcon className="h-5 w-5" />
           </button>
         </Form>
         <ul>
@@ -116,19 +93,11 @@ export default function Index() {
                       className={[
                         snippet.favorite ? "text-amber-500" : "text-slate-400",
                       ]}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
+                      <StarIcon
                         className="h-5 w-5"
-                        viewBox="0 0 24 24"
-                        strokeWidth={2}
                         stroke={snippet.favorite ? "none" : "currentColor"}
-                        fill={snippet.favorite ? "currentColor" : "none"}>
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                        />
-                      </svg>
+                        fill={snippet.favorite ? "currentColor" : "none"}
+                      />
                     </span>
                   </span>
                   <span className="block text-slate-400 text-sm">
--- app/routes/snippets/$snippetId.jsx
+++ app/routes/snippets/$snippetId.jsx
@@ -6,6 +6,7 @@ import {
   redirect,
   useSearchParams,
 } from "remix";
+import { TrashIcon, StarIcon } from "@heroicons/react/outline";
 import connectDb from "~/db/connectDb.server.js";
 
 export async function loader({ params }) {
@@ -51,19 +52,11 @@ export default function SnippetPage() {
                 "block mr-2 hover:text-slate-700 transition-colors",
                 snippet.favorite ? "text-amber-500" : "text-slate-400",
               ].join(" ")}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
+              <StarIcon
                 className="h-6 w-6"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
                 stroke={snippet.favorite ? "none" : "currentColor"}
-                fill={snippet.favorite ? "currentColor" : "none"}>
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                />
-              </svg>
+                fill={snippet.favorite ? "currentColor" : "none"}
+              />
             </button>
           </Form>
           <h1 className="text-2xl font-bold">{snippet.title}</h1>
@@ -76,19 +69,7 @@ export default function SnippetPage() {
             title="Delete"
             aria-label="Delete"
             className="transition-colors text-slate-400 hover:text-red-600">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-              />
-            </svg>
+            <TrashIcon className="h-5 w-5" />
           </button>
         </Form>
       </div>
