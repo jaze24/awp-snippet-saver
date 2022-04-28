d0666371dd2acc6c4c5f8ac57b19a23f1ace017b
Dan Okkels Brendstrup <dob@eaaa.dk>
2022-04-28 22:32:30 +0200

    feat: add webmanifest and icon files

--- app/root.jsx
+++ app/root.jsx
@@ -8,18 +8,50 @@ import {
 } from "remix";
 import styles from "~/tailwind.css";
 
+const THEME_COLOR = "#fafafa";
+
 export const links = () => [
   {
     rel: "stylesheet",
     href: styles,
   },
+  {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    href: "/icons/apple-touch-icon.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/icons/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/icons/favicon-16x16.png",
+  },
+  {
+    rel: "mask-icon",
+    color: THEME_COLOR,
+    href: "/icons/safari-pinned-tab.svg",
+  },
+  {
+    rel: "manifest",
+    href: "/site.webmanifest",
+  },
 ];
 
 export function meta() {
   return {
-    charset: "utf-8",
-    title: "AWP Snippet Saver",
-    viewport: "width=device-width,initial-scale=1",
+    "charset": "utf-8",
+    "title": "Snippet Saver",
+    "viewport": "width=device-width,initial-scale=1",
+    "apple-mobile-web-app-title": "Snippet Saver",
+    "application-name": "Snippet Saver",
+    "msapplication-TileColor": THEME_COLOR,
+    "theme-color": THEME_COLOR,
   };
 }
 
