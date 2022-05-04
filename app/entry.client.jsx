9141190bb98155a94b5c6d5f2c5c5004c815ab36
Dan Okkels Brendstrup <dob@eaaa.dk>
2022-05-04 23:30:17 +0200

    refactor: SW waitUntil for caching

--- app/entry.client.jsx
+++ app/entry.client.jsx
@@ -5,10 +5,8 @@ hydrate(<RemixBrowser />, document);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js", { type: "module" })
-      .catch((error) => {
-        console.error("Service worker registration failed", error);
-      });
+    navigator.serviceWorker.register("/sw.js").catch((error) => {
+      console.error("Service worker registration failed", error);
+    });
   });
 }
--- public/sw.js
+++ public/sw.js
@@ -8,36 +8,7 @@ self.addEventListener("activate", (event) => {
   console.log("SW activated", event);
 });
 
-async function handleFetch(event) {
-  // Serve HTML responses network-first
-  if (event.request.destination === "document") {
-    try {
-      const documentCache = await caches.open(DOCUMENT_CACHE);
-      const response = await fetch(event.request);
-      if (response.status >= 200 && response.status < 400) {
-        await documentCache.put(event.request, response.clone());
-        return response;
-      } else {
-        return (
-          (await documentCache.match(event.request)) ??
-          new Response("Huh?", { status: 404 })
-        );
-      }
-    } catch (error) {
-      return (
-        (await caches.match(event.request)) ??
-        new Response("Huh?", { status: 404 })
-      );
-    }
-  }
-
-  return fetch(event.request);
-}
-
 self.addEventListener("fetch", (event) => {
-  if (event.request.method.toLowerCase() !== "get") {
-    return;
-  }
   console.log(
     "Fetch: %s %s %s %o",
     event.request.method,
@@ -45,5 +16,37 @@ self.addEventListener("fetch", (event) => {
     event.request.destination,
     event.request
   );
-  event.respondWith(handleFetch(event));
+
+  // We only want to handle GET requests
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  // HTML --------------------------------------------------
+  // Cache-first, fallback to network (and then cache it)
+  if (isHtmlRequest(event.request)) {
+    event.respondWith(
+      (async function () {
+        const cachedResponse = await caches.match(event.request);
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        const networkResponse = await fetch(event.request);
+        if (networkResponse.ok) {
+          const clonedResponse = networkResponse.clone();
+          event.waitUntil(
+            (async function () {
+              const documentCache = await caches.open(DOCUMENT_CACHE);
+              return documentCache.put(event.request, clonedResponse);
+            })()
+          );
+        }
+        return networkResponse;
+      })()
+    );
+  }
 });
+
+function isHtmlRequest(request) {
+  return request.destination === "document";
+}
